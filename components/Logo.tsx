81059361345e4b49267b59773144a896f9c27c36
Maxime Boucher <maxime@maximeboucher.com>
2025-10-18 20:49:02 -0600

    Add logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,17 @@
-import { Calendar } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 export function Logo() {
   return (
     <Link href="/">
-      <div className="flex items-center gap-2">
-        <Calendar className="size-6 text-primary" />
-        <h1 className="text-xl font-semibold">CourseCal</h1>
+      <div className="flex items-center gap-3">
+        <Image
+          src="/coursecal-logo.svg"
+          alt="CourseCal logo"
+          width={36}
+          height={36}
+        />
+        <h1 className="text-2xl font-semibold">CourseCal</h1>
       </div>
     </Link>
   );
