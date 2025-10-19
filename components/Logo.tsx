61e78e467d9a2c9d45df43e0c53e6c4d9c9565fe
Maxime Boucher <maxime@maximeboucher.com>
2025-10-19 08:52:39 -0600

    Update logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 export function Logo() {
   return (
     <Link href="/">
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-2.5">
         <Image
           src="/coursecal-logo.svg"
           alt="CourseCal logo"
           width={36}
           height={36}
+          className="size-8 md:size-9"
         />
         <h1 className="text-2xl font-semibold">CourseCal</h1>
       </div>
