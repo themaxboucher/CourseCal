704a2fad1c8a75186fab5f25988b6bc6609f8c39
Maxime Boucher <maxime@maximeboucher.com>
2026-01-09 14:02:40 -0700

    Polish landing page footer

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
-export function Logo() {
+interface LogoProps {
+  className?: string;
+}
+
+export function Logo({ className }: LogoProps) {
   return (
-    <Link href="/">
-      <div className="flex items-center gap-2.5">
-        <Image
-          src="/coursecal-logo.svg"
-          alt="CourseCal logo"
-          width={36}
-          height={36}
-          className="size-8 md:size-9"
-        />
-        <h1 className="text-2xl font-bold">CourseCal</h1>
-      </div>
+    <Link href="/" className={cn("flex items-center gap-2.5", className)}>
+      <Image
+        src="/coursecal-logo.svg"
+        alt="CourseCal logo"
+        width={36}
+        height={36}
+        className="size-8 md:size-9"
+      />
+      <h1 className="text-2xl font-bold">CourseCal</h1>
     </Link>
   );
 }
--- components/landing-page/Footer.tsx
+++ components/landing-page/Footer.tsx
@@ -2,9 +2,22 @@ import { Logo } from "../Logo";
 
 export function Footer() {
   return (
-    <footer className="flex flex-col gap-2 max-w-[75rem] mx-auto md:px-8 px-4">
-      <div className="w-full py-6 space-y-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-2 border-t-2">
-        <Logo />
+    <footer className="flex flex-col items-center gap-2 max-w-[75rem] mx-auto md:px-8 px-4">
+      <div className="py-6 w-full space-y-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-2 border-t-2">
+        <div className="flex flex-col gap-2 mb-0">
+          <Logo className="mb-0" />
+          <div className="text-xs font-medium text-muted-foreground mb-0">
+            Made by{" "}
+            <a
+              href="https://maxboucher.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sky-500 hover:underline"
+            >
+              Max Boucher
+            </a>
+          </div>
+        </div>
         <div className="text-xs text-muted-foreground">
           © 2026 Maxime Boucher. All rights reserved.
         </div>
